9e8ed57c2f1dd20a21fd3cab457f3d9e7e10f863
maxime frnd <maxime.freund@mmibordeaux.com>
2020-03-09 19:49:57 +0100

    git add cs files

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Crypto
+{
+    class Program
+    {
+        
+        public static char cryptage(char ch, int cle)
+        {
+           if (!char.IsLetter(ch))
+            {
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch+cle)-d)%26)+d);
+        }
+
+        public static string encryptage( string entree, int cle)
+        {
+            string sortie = string.Empty;
+
+            foreach (char ch in entree)
+            sortie += cryptage(ch,cle);
+
+            return sortie;
+        }
+
+        public static string decryptage( string entree)
+        {
+            return encryptage(entree,26);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre phrase");
+            string PhraseUser = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Entrez votre cle");
+            int cle = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Encryptage");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine(".");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine(".");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine(".");
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Donnees encryptees : ");
+            string textEncrypte = encryptage(PhraseUser,cle);
+            Console.WriteLine(textEncrypte);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Donnees decryptees");
+            string texteDecrypte = decryptage(PhraseUser);
+            Console.WriteLine(texteDecrypte);
+
+            Console.WriteLine("\n");
+
+        }
+    }
+}
