16f691ec7076c253544dae2ec3e13cb140d2de59
maxime frnd <maxime.freund@mmibordeaux.com>
2020-03-10 21:50:03 +0100

    mid dev

--- Program.cs
+++ Program.cs
@@ -4,7 +4,67 @@ namespace Crypto
 {
     class Program
     {
-        /*public static char cryptage(char ch, int k)
+
+        static void Main( string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = mainMenu();
+            }
+        }
+
+        static bool mainMenu()
+        {
+            Console.WriteLine("------------------\n");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Choisissez un programme à lancer: \n");
+            Console.WriteLine("------------------\n");
+            Console.WriteLine("1) CesarEncrypte");
+            Console.WriteLine("2) CesarDecrypte");
+            Console.WriteLine("3) Quitter");
+            Console.Write("\r\nSelectionnez une option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1" : 
+                    cesar();
+                    return true; 
+                case "2" :
+                    cesarFreq();
+                    return true;
+                case "3" :
+                    return false;
+                default:
+                    return true;
+            }
+
+        }
+
+        public static void cesar ()
+        {
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Entrez votre phrase : ");
+            string m = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Entrez votre clé : ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            string c = encryptage(m,k);
+            Console.WriteLine("Code : "+c);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("----------");
+
+        }
+
+        public static char cryptage(char ch, int k)
         {
             if (!char.IsLetter(ch))
             {
@@ -28,36 +88,57 @@ namespace Crypto
             return sortie;
         }
 
-        public static void Cesar (String[] args)
+        public static void cesarFreq ()
         {
+            Console.WriteLine("\n Entrez le message a decoder : ");
+            string c = Console.ReadLine();
 
-            Console.WriteLine("\n");
+            int cleRelative = freq(c);
+            
+            int cle = rela(cleRelative);
 
-            Console.WriteLine("Entrez votre phrase : ");
-            string m = Console.ReadLine();
+            string m = decrypte(cle); 
 
-            Console.WriteLine("\n");
+        }
 
-            Console.WriteLine("Entrez votre clé : ");
-            int k = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\n");
-
-            string c = encryptage(m,k);
-            Console.WriteLine("Code : "+c);
-            Console.WriteLine("Message : "+m);
-
-            Console.WriteLine("\n");
-            Console.WriteLine("----------");
-            Console.WriteLine("\n");
-        }*/
-
-        public static void Main(String[] Args)
+        public static int freq(string c)
         {
-            String c = "giwxexvezivwhipevkiwkvmppiwuyipiwjiqippiwhygerxsrgsrxiqtpemirxyrtymwwerxksvmppiwerwwsygmhyuyirhmvexsrezigmqtyhiyvgiwgsqqiviwpsvkremirxqiqiyrirhvsmxtvigmwuyivmksyviywiqirxqeqiviqehijirhyhirsqqivmgmkevieyksvmppixsyxegsytpetvmwsrfmirgpswisyzmzemxpifipermqepwsyzvisrrwemxtsyvuysmniwyttswiuysrezemxhypejivqivqeppiwmrkiirwsvxerxhiwegekihmxgiwxeynsyvhlymuyinpitivhwmptevpemxhiwsrtygipekizsywezmidhizmriniwtivikevieyksvmppiptexvsrhipeqirekivmigvmemxitivhyrsqhirsqgiwxewwsqqerxgevpiksvmppireneqemwgsrryhikyirsrhiwuyipejiqmrmriirkiergiwyxuyipiwmrkiixemxtygieyeypmiyhitvsjmxivhipeglergiippijmxjiyhiwhiybjywieybkevieyksvmppigippiwpeqiqiuymrekyivipigsyzemirxhyrmphigmhijymvirxtvsyzerxuyippiwrezemirxkyivihipewymxiherwpiwmhiiwheyxerxtpywzemriixemxpiyvgvemrxiuyipiksvmppiiwxyrpyvsrwytivmiyveplsqqiherwpixvimrxifmirhiwjiqqiwzsywpihmvsrxkevieyksvmppixsyxpiqsrhiwitvigmtmxilsvwhexximrxihywmrkiirvyxweyjyrizmippihigvitmxiixyrniyrinykiirfsmwfvyxzscerxuyixsyxiwwihivsfirxpiuyehvyqerieggipivewsrherhmriqirxzivwpiwvsfiwhipezmimppiixhyqekmwxvexkevieyksvmppifelwsytmvemxpegirxiremviuysrtymwwiirgsviqihiwmvivgiwivemxibxvesvhmremviixtsyvxsyxhmvimriwtivipinykitirwemxmqtewwmfpiuysrqitvirritsyvyrikyirsrgiwxgsqtpixiqirxmqtswwmfpipewymxipymtvsyzeuyirsrkevieyksvmppiwyttswiduyipyrhizsywtymwwiixvigsqqipiwmrkisfpmkihizmspivyrnykisyyriergixvipiuyipglsmwmvemxmphiwhiybuyyriepxivrexmzitevimppiyrhigiwuyexviwnsyvwqiglsmigiwxnirwymwgsrzemrgypezmimppiuymwivepsfnixhiqsrglsmbkevieyksvmppiqemwtevqepliyvwmpiksvmppieybniybhipeqsyvzeyxwsrtvmbsrwemxuyirvizerglimprifvmppirmtevpiksxrmtevpiwtvmxpsvweypmiyhstxivtsyvpezmimppigsqqipeyvemxjemxrmqtsvxiuymmpwemwmxpinykiepsvimppiixpirxvereherwyrqeuymwkevieyksvmppipewymxiwivemxhipigxefpiqepliyviywiqirxniritiybtewpehmviixgiwxvikvixxefpiersyweyvemxjemxvmviyrtiygevpinykieyqsqirxwytviqigvmemxqeqertpiyvemxfieygsytgsqqiplsqqieyuyippinsyvqiqimpezemxjemxxverglivpigsykevieyksvmppi";
-            
+            int k = 0;
+            int[] f = new int[26];
+            char[] ch = c.ToCharArray();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                f[ch[i]-97]++;
+            }
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                if(f[i]>f[k])
+                {
+                    k = i;
+                }
+            }
+
+            return k;
+        }
+
+        public static int rela(int k)
+        {
+            int k2 = 0;
+
+            k2 = (k-4)+101;
+
+            return k2;
+        }
+
+        public static string decrypte(int k)
+        {
+            string sortie = string.Empty;
+
             
 
+            return sortie;
         }
 
     }
