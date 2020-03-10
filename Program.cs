fe4654872bb6843d8da009f3e2bf89a5f087ab33
maximefrnd <43884145+maximefrnd@users.noreply.github.com>
2020-03-10 15:22:40 +0100

    simpler code

--- Program.cs
+++ Program.cs
@@ -4,70 +4,52 @@ namespace Crypto
 {
     class Program
     {
-        
-        public static char cryptage(char ch, int cle)
+        public static char cryptage(char ch, int k)
         {
-           if (!char.IsLetter(ch))
+            if (!char.IsLetter(ch))
             {
                 return ch;
             }
 
-            char d = char.IsUpper(ch) ? 'A' : 'a';
-            return (char)((((ch+cle)-d)%26)+d);
+            return (char)(ch+k);
         }
 
-        public static string encryptage( string entree, int cle)
+        public static string encryptage(string entree, int k)
         {
+
             string sortie = string.Empty;
 
             foreach (char ch in entree)
-            sortie += cryptage(ch,cle);
+            {
+                sortie += cryptage(ch,k);
+            }
 
             return sortie;
         }
 
-        public static string decryptage(string entree, int cle)
+        public static void Main (String[] args)
         {
-            return encryptage(entree,26-cle);
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Entrez votre phrase");
-            string textUser = Console.ReadLine();
 
             Console.WriteLine("\n");
 
-            Console.WriteLine("Entrez votre cle");
-            int cle = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entrez votre phrase : ");
+            string m = Console.ReadLine();
 
             Console.WriteLine("\n");
 
-            Console.WriteLine("Encryptage");
-            System.Threading.Thread.Sleep(500);
-            Console.WriteLine(".");
-            System.Threading.Thread.Sleep(500);
-            Console.WriteLine(".");
-            System.Threading.Thread.Sleep(500);
-            Console.WriteLine(".");
-            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Entrez votre clé : ");
+            int k = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("\n");
 
-            Console.WriteLine("Donnees encryptees : ");
-            string textEncrypte = encryptage(textUser,cle);
-            Console.WriteLine(textEncrypte);
-
-            Console.WriteLine("\n");
-
-            Console.WriteLine("Donnees decryptees : ");
-            string textDecrypte = decryptage(textEncrypte,cle);
-            Console.WriteLine(textDecrypte);
+            string c = encryptage(m,k);
+            Console.WriteLine("Code : "+c);
+            Console.WriteLine("Message : "+m);
 
             Console.WriteLine("\n");
             Console.WriteLine("----------");
             Console.WriteLine("\n");
-
         }
+
     }
 }
